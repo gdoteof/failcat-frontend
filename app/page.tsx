14678f77e8f551a13f289d9a44db4b64c8342988
gdoteof <gdoteof@gmail.com>
2023-05-14 03:26:39 -0400

    striaght to main

--- app/page.tsx
+++ app/page.tsx
@@ -4,28 +4,50 @@ import { Car, CarModel, Dealer } from "./models";
 import React from "react";
 import { useSearchParams } from "next/navigation";
 import Link from "next/link";
-import { Table } from "@nextui-org/react";
+import { Button, Table } from "@nextui-org/react";
+import { MdPictureAsPdf } from "react-icons/md";
 
 // add perpage and offset to fetchCars as query params to api/cars
-async function fetchCars(perPage: number, offset: number): Promise<Car[]> {
+async function fetchCars(
+  perPage: number,
+  offset: number,
+  dealer: string | null
+): Promise<Car[]> {
+  const maybeDealerQuery = dealer ? `&dealer=${dealer}` : "";
   return await fetch(
-    `https://fc-api.vteng.io/api/cars?perpage=${perPage}&offset=${offset}`
+    `https://fc-api.vteng.io/api/cars?perpage=${perPage}&offset=${offset}${maybeDealerQuery}`
   ).then((res) => res.json());
 }
 
 export default function Page() {
   const searchParams = useSearchParams();
   // use fetchCars(perPage, offset) to get data to show
-  const perPage: number = parseInt(searchParams.get("perPage") ?? "") || 10;
   const offset: number = parseInt(searchParams.get("offset") ?? "") || 0;
 
   const [cars, setCars] = React.useState<Car[]>([]);
+  const [perPage, setPerPage] = React.useState<number>(
+    parseInt(searchParams.get("perPage") ?? "") || 25
+  );
+  const [dealer, setDealer] = React.useState<string | null>(
+    searchParams.get("dealer")
+  );
   React.useEffect(() => {
-    fetchCars(perPage, offset).then((cars) => {
+    fetchCars(perPage, offset, dealer).then((cars) => {
       setCars(cars);
       console.log(cars);
     });
-  }, [perPage, offset]);
+  }, [perPage, offset, dealer]);
+
+  const highlightLastSix = (vin: string) => {
+    const lastSix = vin.slice(-6);
+    const rest = vin.slice(0, -6);
+    return (
+      <>
+        {rest}
+        <span style={{ fontWeight: "bold" }}>{lastSix}</span>
+      </>
+    );
+  };
 
   return (
     <div>
@@ -37,29 +59,45 @@ export default function Page() {
         }}
       >
         <Table.Header>
-          <Table.Column>Id</Table.Column>
+          <Table.Column>Serial Number</Table.Column>
           <Table.Column>Vin</Table.Column>
           <Table.Column>Ext Color</Table.Column>
           <Table.Column>Int Color</Table.Column>
           <Table.Column>Car Model</Table.Column>
           <Table.Column>Opt Code</Table.Column>
-          <Table.Column>Ship To</Table.Column>
-          <Table.Column>Sold To</Table.Column>
-          <Table.Column>Serial Number</Table.Column>
+          <Table.Column>Dealer</Table.Column>
           <Table.Column>Model Year</Table.Column>
         </Table.Header>
         <Table.Body>
           {cars.map((car) => (
-            <Table.Row key={car.id}>
-              <Table.Cell>{car.id}</Table.Cell>
-              <Table.Cell>{car.vin}</Table.Cell>
+            <Table.Row key={car.serial_number}>
+              <Table.Cell>{car.serial_number}</Table.Cell>
+              <Table.Cell>
+                {highlightLastSix(car.vin)}
+                <a
+                  href={`https://failcat-rust.vteng.io/window-sticker/${car.vin}`}
+                >
+                  <MdPictureAsPdf />
+                </a>
+              </Table.Cell>
               <Table.Cell>{car.ext_color}</Table.Cell>
               <Table.Cell>{car.int_color}</Table.Cell>
               <Table.Cell>{car.car_model}</Table.Cell>
               <Table.Cell>{car.opt_code}</Table.Cell>
-              <Table.Cell>{car.ship_to}</Table.Cell>
-              <Table.Cell>{car.sold_to}</Table.Cell>
-              <Table.Cell>{car.serial_number}</Table.Cell>
+              <Table.Cell>
+                <Link
+                  className="px-12"
+                  href={{
+                    query: {
+                      perPage: perPage,
+                      offset: offset + perPage,
+                      dealer: car.ship_to,
+                    },
+                  }}
+                >
+                  {car.ship_to}
+                </Link>
+              </Table.Cell>
               <Table.Cell>{car.model_year}</Table.Cell>
             </Table.Row>
           ))}
